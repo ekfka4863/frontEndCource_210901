a872baeca1c37a4d7d51bf77b3ac60549569e3db
ekfka4863 <70094833+ekfka4863@users.noreply.github.com>
2021-11-28 02:56:02 +0900

    Commit: HW -> modal window -> display: none; opacity: 0; -> DOM manipulation

--- TIL/TIL(2021-11-26)_미완_JS_jQuery_slideUp_slideDown_tapMenu.md
+++ TIL/TIL(2021-11-26)_미완_JS_jQuery_slideUp_slideDown_tapMenu.md
@@ -4,10 +4,53 @@ date: 2021-11-26-Friday
 
 # TIL
 - 📝 오늘 배운 내용 ✏️ : 
-  - [x] ``       
-    -     
-  - [x] ``            
-  - [x] jQuery 사용할 때 `(function($) {})(jQuery)`를 사용하는 이유/ 역할       
+  - [x] `tab menu` 구현하기 using jQuery        
+  <details>
+    <summary>CLICK ME TO SEE CODES!</summary>
+
+    ```html
+    ```
+    <br />
+
+    ```scss
+    ```
+    <br />
+
+    ```css
+    ```
+    <br />
+
+    ```js
+    ```
+    <br />
+
+  </details>
+  <br />
+  - [x] jQuery 사용할 때 `(function($) {})(jQuery)`를 사용하는 이유/ 역할     
+    - 일단, 원래 jQuery를 사용하기 위한 기본 문법이 있다       
+    ```js
+      $document.ready(function() {}); 
+    ```
+    - 원래 모든 jQuery는 위의 코딩으로부터 시작된다.       
+    위의 표현은 순수 자바스크립트로 하자면 `document.onload()`와 같은 표현이라고 생각하고, 
+    해석은 `html 문서가 로딩이 되면...`이라고 이해하면 된다. 
+    - 하지만 위의 코드말고 우리는 jQuery 코드를 좀 더 줄여서 
+      - `$(function(){})` 라고도 표현한다.       
+      또한, 위의 shorthand를 즉시실행함수(cf. IIFE, Immediately Invoked Function Expression)
+      로 바꿔서 
+      - `(function(){})($)`    
+      또는     
+      - `(function($){})()` 처럼 사용 할 수도 있다.      
+      이때, 즉시 호출함수로 jQuery 코딩의 시작을 알리는 코드를 작성하는 이유는 
+      다른 프로그래밍 언어에서는 달러사인($)이 다른 의미로 사용되기도 하고 브라우저에서는 여러개의 자바스크립트 파일들에 있는 글로벌 변수들이 존재하는데, 이런 이유때문에 IIFE로 제이쿼리가 실행되는 범위(scope)를 분리(isolate)하여 안전하게 선택자를 혼돈하지 않고 변수 사용/코드를 컴파일할 수 있게 해주는 것이다.      
+
+
+
+<br />
+<br />
+
+- 📝 오늘 복습할 내용 ✏️ : 
+  - [x] `timer` 복습             
 
   <!-- https://okky.kr/article/279353 -->
 
@@ -41,6 +84,12 @@ date: 2021-11-26-Friday
 
 - cf.  
   - ✨ Only 선생님's 강의 ✨
+  - about:blank
+  - https://www.quora.com/What-does-dollar-sign-mean-in-programming
+  - https://theqoop.tistory.com/295
+  - https://www.computerhope.com/jargon/d/dollarsign.htm
+  - https://stackoverflow.com/questions/41809631/jquery-function-vs-function
+  - 
   
 </detials>   
 
---
+++ js_practice/test_11.js
@@ -0,0 +1,131 @@
+// test_11.js
+
+// DOM 
+var imgCardWrapper = document.querySelector('.img_card_wrapper');
+var imgCards = imgCardWrapper.querySelectorAll('.img_card');   // NodeList 
+
+var modalBg = document.querySelector('.modal_bg');
+var modalConZoomed = modalBg.querySelector('.modal_con_zoomed');
+var closeBtnWrapper = document.querySelector('.close_btn_wrapper');
+var closeBtn = closeBtnWrapper.querySelector('button');
+
+
+
+// event
+
+// event 1: img_card 클릭시 모달창 display: block; opacity: 100%; -> js로 클릭 이벤트 발생시 
+// nodelist인 imgCards의 접근하기 위해 ... 
+imgCards = Array.prototype.slice.call(imgCards);  // 배열로 만들기 
+imgCards.forEach(elem => {
+
+  var buttonElem = elem.querySelector('button');
+  var textValue = buttonElem.querySelector('span').textContent;
+  // console.log(textValue, typeof textValue);    // img_1  img_2  img_3 -> string
+
+  // event 1 구현
+  buttonElem.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // console.log('it works!');
+    modalBg.style.display = 'block';
+
+    // opacity
+    var currentOp = Number(modalBg.style.opacity);  // 0
+    // console.log(currentOp);
+    modalBg.style.opacity = '1';
+    currentOp = modalBg.style.opacity;
+    // console.log(currentOp);
+
+
+    // ---------------------------------
+    // ---------------------------------
+
+
+    // event 3: 이미지 삽입 이벤트 -> 변수 선언? scss 리스트화? 
+    
+    var imageUrls = [
+      "https://images.unsplash.com/photo-1625758600922-4085dd859395?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aWNlY3JlYW18ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+      "https://images.unsplash.com/photo-1623686088099-d74b7a1d6754?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2hyaXNtYXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60",
+      "https://images.unsplash.com/photo-1637988936322-016302f3226d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+    ];
+    
+    // 여기부터 다시 !! 
+    // check!!!
+    // check!!!
+    // check!!!
+    // check!!!
+    // check!!!
+    // check!!!
+    console.log(textValue);
+    // console.log(modalConZoomed.style.backgroundImage);
+    console.log(getComputedStyle(modalConZoomed).backgroundImage);
+
+    // textValue로 이미지가 들어갈 자리를 파악 -> url을 각 카드마다 따로 지정!
+    switch (textValue) {
+      case 'img_1':
+        modalConZoomed.style.backgroundImage = imageUrls[0];
+        break;
+      case 'img_2':
+        modalConZoomed.style.backgroundImage = imageUrls[1];
+        break;
+      case 'img_3':
+        modalConZoomed.style.backgroundImage = imageUrls[2];
+        break;
+      default:
+        modalConZoomed.style.backgroundColor = '#fe2';
+        break;
+    }// switch문
+
+  });
+
+
+
+});
+
+
+
+// event 2: 모달창 내 버튼 x 클릭시 display: none; opacity: 0; -> js로 클릭 이벤트 발생시 
+closeBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  // opacity
+  // var originalOp = Number(getComputedStyle(modalBg).opacity);   // 1
+
+  // console.log(modalBg.style.opacity);         // ''
+  // console.log(Number(modalBg.style.opacity));  // 0
+  var currentOp = Number(modalBg.style.opacity);  // 0
+
+  var removeOpacity = setInterval(function() {
+    if (currentOp === 0) {   // ''거나 0, false면 ...
+      modalBg.style.opacity = '1';
+      currentOp = modalBg.style.opacity;
+    } 
+    
+    if (currentOp > 0) {
+      modalBg.style.opacity -= 0.1;
+      currentOp = modalBg.style.opacity;
+    } else {
+      clearInterval(removeOpacity);
+      modalBg.style.display = 'none';
+      modalBg.style.opacity = '0';
+      currentOp = modalBg.style.opacity;
+    }
+  }, 50);
+  // https://stackoverflow.com/questions/29017379/how-to-make-fadeout-effect-with-pure-javascript
+
+});
+
+
+
+// 이미지 삽입 이벤트 -> 변수 선언? scss 리스트화? 
+// if (imgCards)
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+// 주말 숙제 
+// 샘플 사이트: fancybox -> http://fancybox.net/
+// 샘플 사이트 2: https://webpublisher-jquery-sample.vercel.app/html/ex_03_modal.html
+// hw
+// cpacity: 100%; -> opacity: 0; display: none;
+// display: block; -> opacity: 100%;
+// 이런 구조로 만들기 
